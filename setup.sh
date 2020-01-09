1cca70590d3e733605d688abd6c069bd1aefe7b0
astherath <farceriv@gmail.com>
2020-01-08 20:42:46 -0500

    added bashrc

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!bin/bash
 
 # copy vimrc to ~/.vimrc
-cat vimrc >> ~/.vimrc
+cat vimrc > ~/.vimrc
+
 # install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# copy bashrc to ~/.bashrc
+cat bashrc >> ~/.bashrc
